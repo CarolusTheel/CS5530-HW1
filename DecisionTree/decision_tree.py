62f7ebe3f0d8bdf3edacfb0dcc47d52d015c6b8d
CarolusTheel <carolus.theel@gmail.com>
2023-09-20 14:33:31 -0600

    start of implementation of node split

--- DecisionTree/decision_tree.py
+++ DecisionTree/decision_tree.py
@@ -5,7 +5,7 @@ class node:
     inputDomain = []
 
     isLeaf = False
-    leafVal = None
+    mostCommonVal = None
 
     inputs = []
     outputs = []
@@ -19,8 +19,23 @@ class node:
         self.outputs = [entry[-1] for entry in data]
         self.heuristic = heuristic
 
-    def split(self):
-        pass
+    def split(self, maxDepth):
+        if maxDepth < 0:
+            return
+        elif maxDepth == 1:
+            self.isLeaf = True
+            maxCount = 0
+            mostCommonVal = self.outputs[0]
+            for o in self.outputs:
+                count = self.outputs.count(o)
+                if(count > maxCount):
+                    maxCount = count
+                    mostCommonVal = o
+        
+        # run ID3 on node
+        else:
+            pass
+        
 
     def printNode(self):
         print("ID: ", self.bestSplit, " --- ", len(self.nextNodes.keys()), " child nodes", sep="")
